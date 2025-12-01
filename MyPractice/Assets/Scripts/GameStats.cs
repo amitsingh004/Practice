e4ce58e7c08919eec8ae6e7a9292935eaaea769c
amitsingh004 <amitkrsingh004@gmail.com>
2025-12-01 23:28:48 +0530

    Revert "Reorganize project folder structure"
    This reverts commit 65a1e180ced9822a2e85e22142ed72e5a1416a65.

---
+++ MyPractice/Assets/Scripts/GameStats.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System;
+public class GameStats 
+{
+    private int score = 0;
+    private int totalPairs;
+    private int matchedPairs = 0;
+    private int turnCount = 0;
+    public event Action<int> OnScoreChanged;
+    public event Action<int> OnTurnCountChanged;
+    public event Action OnGameWon;
+
+    public void Initialize(int totalPairs)
+    {
+        this.totalPairs = totalPairs;
+        score = 0;
+        matchedPairs = 0;
+        Debug.Log("GameStats initialized.");
+    }
+    public void ResetScore()
+    {
+        score = 0;
+        matchedPairs = 0;
+        OnScoreChanged?.Invoke(score);
+
+    }
+    public void ResetTurnCount()
+    {
+        turnCount = 0;
+        OnTurnCountChanged?.Invoke(turnCount);
+        Debug.Log("Turn count reset to 0.");
+    }
+    public void IncrementTurnCount()
+    {
+        turnCount++;
+        Debug.Log($"Turn count increased to {turnCount}");
+        OnTurnCountChanged?.Invoke(turnCount);
+    }
+    public void AddScore(int amount)
+    {
+        score += amount;
+        matchedPairs++;
+        Debug.Log($"Score increased by {amount}. Current score: {score}");
+        OnScoreChanged?.Invoke(score);
+        CheckGameWin(); 
+    
+    }
+    public void CheckGameWin()
+    {
+        Debug.Log($"Matched pairs: {matchedPairs}/{totalPairs}");
+        if (matchedPairs >= totalPairs)
+        {
+            OnGameWon?.Invoke();
+            Debug.Log("All pairs matched! Game won!");
+        }
+    }
+}
