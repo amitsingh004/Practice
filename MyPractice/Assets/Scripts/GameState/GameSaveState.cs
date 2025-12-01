98ad60d89d9d2da9dc633e8a8c16c843e09c7c82
amitsingh004 <amitkrsingh004@gmail.com>
2025-12-02 00:00:03 +0530

    added  save/load system additional files

---
+++ MyPractice/Assets/Scripts/GameState/GameSaveState.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public class GameSaveState
+{
+    public int score;
+    public int turns;
+    public Vector2Int layoutSize;
+    public int[] cardStates; // 0: Closed, 1: Opening, 2: Open, 3: Closing, 4: Matched
+    public int[] cardIds; // Unique IDs for each card
+
+}
